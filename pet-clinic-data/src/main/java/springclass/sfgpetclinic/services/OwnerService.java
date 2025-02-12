af329cd57b8483d6ac914a64d3b60c964855b61d
jardelbrando <jardelbrando@gmail.com>
2025-02-12 15:23:07 -0300

    Implement Interfaces for services. Closes#5

---
+++ pet-clinic-data/src/main/java/springclass/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,14 @@
+package springclass.sfgpetclinic.services;
+
+import springclass.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+
+}
