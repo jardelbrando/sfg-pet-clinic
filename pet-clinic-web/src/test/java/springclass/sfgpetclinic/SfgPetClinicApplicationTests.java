3e10672a685469d169860169bd44f8e4d583552c
jardelbrando <jardelbrando@gmail.com>
2025-02-12 10:10:24 -0300

    Preparing project

--- pet-clinic-web/src/test/java/springclass/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springclass/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -2,6 +2,7 @@ package springclass.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import springclass.sfgpetclinic.SfgPetClinicApplication;
 
 @SpringBootTest
 class SfgPetClinicApplicationTests {
