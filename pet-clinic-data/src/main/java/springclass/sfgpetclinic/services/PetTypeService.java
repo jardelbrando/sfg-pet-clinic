133fce28ed4103109d208e434f6826e9649c4438
jardelbrando <jardelbrando@gmail.com>
2025-02-27 10:35:47 -0300

    Added PetTypeMapService. closes #29

---
+++ pet-clinic-data/src/main/java/springclass/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package springclass.sfgpetclinic.services;
+
+import springclass.sfgpetclinic.model.PetType;
+
+/**
+ * @author Jardel Brandao
+ * @date 27/02/2025
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/springclass/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,39 @@
+package springclass.sfgpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import springclass.sfgpetclinic.model.PetType;
+import springclass.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+/**
+ * @author Jardel Brandao
+ * @date 27/02/2025
+ */
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
