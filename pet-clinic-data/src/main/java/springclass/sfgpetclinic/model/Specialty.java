ef3f5cb8835f408e2e06b9c7142985a53ef7b3f1
jardelbrando <jardelbrando@gmail.com>
2025-03-14 14:43:40 -0300

    Turning Vet to a JPA entity and mapping it #37.

--- pet-clinic-data/src/main/java/springclass/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/springclass/sfgpetclinic/model/Specialty.java
@@ -1,11 +1,18 @@
 package springclass.sfgpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 /**
  * @author Jardel Brandao
  * @date 26/02/2025
  */
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/springclass/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/springclass/sfgpetclinic/model/Vet.java
@@ -1,12 +1,21 @@
 package springclass.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Vet extends Person{
 
     private Set<Specialty> specialties = new HashSet<>();
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "vet_speacialty",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "specialty_id")
+    )
     public Set<Specialty> getSpecialties() {
         return specialties;
     }
