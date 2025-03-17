ece57e1c7d0cbd99b92083060cd4e0bb4bd1e1db
jardelbrando <jardelbrando@gmail.com>
2025-03-17 10:17:30 -0300

    Adding Visit and Map mapping. closes #38.

--- pet-clinic-data/src/main/java/springclass/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/springclass/sfgpetclinic/model/Visit.java
@@ -1,15 +1,25 @@
 package springclass.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
 /**
  * @author Jardel Brandao
  * @date 26/02/2025
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
